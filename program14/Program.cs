9499883425ca55e81dff93599d90f93c82f4bef2
Yusuf Selim Akdemir <yusakru@gmail.com>
2024-11-11 11:37:58 +0300

    update_0.1

--- program14/Program.cs
+++ program14/Program.cs
@@ -1,29 +1,21 @@
-﻿using System;
+﻿Console.Write("Lütfen bir sayı giriniz: ");
+int limit = Convert.ToInt32(Console.ReadLine());
 
-class Program
+// İlk iki Fibonacci sayısı
+int a = 0, b = 1;
+
+// İlk sayıyı yazdır
+Console.Write(a);
+
+// Limit değerine kadar Fibonacci sayılarını yazdır
+while (b <= limit)
 {
-    static void Main(string[] args)
-    {
-        Console.Write("Lütfen bir sayı giriniz: ");
-        int limit = Convert.ToInt32(Console.ReadLine());
+    Console.Write(", " + b);
 
-        // İlk iki Fibonacci sayısı
-        int a = 0, b = 1;
-
-        // İlk sayıyı yazdır
-        Console.Write(a);
-
-        // Limit değerine kadar Fibonacci sayılarını yazdır
-        while (b <= limit)
-        {
-            Console.Write(", " + b);
-            
-            // Bir sonraki Fibonacci sayısını hesapla
-            int temp = a + b;
-            a = b;
-            b = temp;
-        }
-
-        Console.WriteLine();
-    }
+    // Bir sonraki Fibonacci sayısını hesapla
+    int temp = a + b;
+    a = b;
+    b = temp;
 }
+
+Console.WriteLine();
--- program14/obj/Debug/net8.0/program14.AssemblyInfo.cs
+++ program14/obj/Debug/net8.0/program14.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program14")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69c50aabd08137c0f27c93ed4d26700c30791358")]
 [assembly: System.Reflection.AssemblyProductAttribute("program14")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program14")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
